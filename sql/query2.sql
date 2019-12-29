57d77bb11ba1e867d27814166c8cf19c23793c2e
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-12-28 23:05:56 -0800

    Add example refactored query

--- sql/query2.sql
+++ sql/query2.sql
@@ -1,6 +1,25 @@
 SELECT
-  *
+  cu.customer_id AS id,
+  concat(cu.first_name, ' ', cu.last_name) AS name,
+  a.address AS address,
+  a.postal_code AS `zip code`,
+  a.phone AS phone,
+  sakila.city.city AS city,
+  sakila.country.country AS country,
+  if(cu.active, 'active', '') AS notes,
+  cu.store_id AS sid
 FROM
-  actor
-LIMIT
-  1
+  (
+    (
+      (
+        sakila.customer cu
+        JOIN sakila.address a on((cu.address_id = a.address_id))
+      )
+      JOIN sakila.city on((a.city_id = sakila.city.city_id))
+    )
+    JOIN sakila.country on(
+      (
+        sakila.city.country_id = sakila.country.country_id
+      )
+    )
+  )
